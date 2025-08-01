9cdbb6debb0a9eeb6cfe049540bdf05e323d04fe
Zanderesque <alex.bolyanatz@gmail.com>
2025-08-01 17:01:38 -0600

    updated package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Navbar } from '@/components/Navbar';
-import { Footer } from '@/components/Footer';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
 import { openSans, montserrat } from './services/fonts';
 
 export const metadata: Metadata = {
--- app/services/fonts.js
+++ app/services/fonts.js
@@ -4,10 +4,12 @@ export const openSans = Open_Sans({
   subsets: ["latin"],
   display: "swap",
   weight: ["400", "500", "600"],
+  fallback: ["system-ui", "arial"],
 });
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
   display: "swap",
   weight: ["400", "500", "600", "700"],
+  fallback: ["system-ui", "arial"],
 });
